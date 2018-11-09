a41da898d3662c366601f707e5395d484ba9ad64
Christopher Brown <chbphone55@gmail.com>
2018-11-08 22:03:47 -0700

    Changes to navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    <div class="banner-div" style="width: 200px; position: relative;">
+    <!-- <div class="banner-div" style="width: 200px; position: relative;">
       <img id="banner-img" style="position: relative;" src="./assets/banner.png" alt="banner" />
       <img id="title-img" style="position: absolute; top: 50px; left: 50px;" src="./assets/Jeremy's Garage1.png" alt="title" />
-    </div>
+    </div> -->
     <!-- <img alt="Banner" src="./assets/banner.png">
     <img class="title" alt="jeremysgarage" src="./assets/Jeremy's Garage.png"> -->
 
-    <nav class="navbar">
+    <nav id="nav">
+      <router-link tag='p' to='/' class='title'>Jeremy's Garage</router-link>
       <router-link to="/">Home</router-link>
-      <router-link to="/services">Services</router-link>
       <router-link to="/about">About</router-link>
+      <router-link to="/services">Services</router-link>
       <router-link to="/videos">Videos</router-link>
       <router-link to="/contact">Contact</router-link>
     </nav>
@@ -38,43 +39,70 @@
     padding: 0;
   }
 
-  /* #nav {
-    padding: 30px;
-    text-align: center;
-  }
-
-  #nav a {
-    font-weight: bold;
-    color: #000000a4;
-  }
-
-  #nav a.router-link-exact-active {
-    color: #47aeb8;
-  }  */
-
-
-  .navbar {
-    overflow: hidden;
-    background-color: rgba(0, 0, 0, 0.884);
+  #nav {
     position: fixed;
     top: 0;
+    left: 0;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+
     width: 100%;
+
+    background-color: rgba(0, 0, 0, 0.884);
+
+    z-index: 999;
   }
 
-  .navbar a {
-    float: left;
-    display: block;
+  #nav > .title {
+    margin: auto auto auto 1rem;
+
     color: #f2f2f2;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 15px;
+
+    font-weight: bold;
+    letter-spacing: 1px;
+    word-spacing: 2px;
   }
 
-  .navbar a:hover {
-    background: rgba(46, 158, 192, 0.212);
-    color: rgb(255, 255, 255);
-    border-radius: 15px;
+  #nav > a {
+    position: relative;
+
+    padding: 0.5rem 0.85rem;
+
+    color: #f2f2f2;
+
+    font-size: 1rem;
+    font-family: Roboto;
+    text-decoration: none;
+
+    transition: background-color 0.2s;
+  }
+
+  #nav > a::after {
+    content: '';
+
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+
+    width: 0;
+    height: 2px;
+
+    /* TODO: Change "lightblue" to the theme-color */
+    background-color: lightblue;
+
+    transform: translateX(-50%);
+    transition: width 0.2s;
+  }
+
+  #nav > a:active,
+  #nav > a:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+
+  #nav > a.router-link-exact-active::after {
+    width: 100%;
   }
 
   .main {
