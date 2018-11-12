eb0692bd583517d42ca25507756bbea7fc7dc644
Christopher Brown <chbphone55@gmail.com>
2018-11-12 15:34:52 -0700

    Media query for contact map

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,17 +1,18 @@
 <template>
   <!-- this page will be for contact information, map/location, and a link to contact form -->
   <div class="contact">
+    <h1 class="title">Contact Info</h1>
     <section class="location">
       <article class="hours-of-operation">
-        <h2 class='title'>Hours of Operation</h2>
-        <ul class='hours-list'>
+        <h2 class="title">Hours of Operation</h2>
+        <ul class="hours-list">
           <li>Monday through Friday: 8 AM - 5 PM</li>
           <li>Saturday: by appointment only</li>
           <li>Sundays: Closed</li>
         </ul>
       </article>
 
-      <iframe width="750" height="500" id="gmap_canvas" frameborder="0" scrolling="no" marginwidth="0" marginheight="0"
+      <iframe class="map" frameborder="0" scrolling="no" marginwidth="0" marginheight="0"
         src="https://maps.google.com/maps?q=Jeremys%20garage&t=&z=13&ie=UTF8&iwloc=&output=embed">
         <a href="https://www.pureblack.de"></a>
       </iframe>
@@ -21,15 +22,19 @@
 
 <script>
   export default {
-    name: 'Contact'
+    name: "Contact"
   }
 </script>
 
 <style scoped>
+  .contact > .title {
+    text-align: center;
+    margin: 3rem 0;
+  }
+
   .location {
     display: flex;
     justify-content: center;
-    flex-wrap: wrap;
   }
 
   .hours-of-operation {
@@ -45,5 +50,32 @@
     list-style: none;
     padding: 0;
     text-align: right;
+  }
+
+  .map {
+    min-width: 20rem;
+    max-width: 40rem;
+    width: 70%;
+    height: 25rem;
+  }
+
+  @media (max-width: 999px) {
+    .location {
+      flex-direction: column;
+      align-items: center;
+      padding-bottom: 2rem;
+    }
+
+    .hours-of-operation {
+      margin: 0;
+    }
+
+    .hours-of-operation > .title {
+      text-align: center;
+    }
+
+    .hours-list {
+      text-align: center;
+    }
   }
 </style>
