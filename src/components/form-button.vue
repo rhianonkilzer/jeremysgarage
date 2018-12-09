db0093c2e029f663538c09215cb41edb4d1e7e4f
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-12-09 15:01:30 -0700

    buttons
    Co-authored-by: Christopher Brown <chbphone55@gmail.com>

--- src/components/form-button.vue
+++ src/components/form-button.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="form-button">
-
-
-    </div>
+    <button class="form-button">
+        <slot></slot>
+    </button>
 </template>
 
 
 <script>
     export default {
         name: 'form-button',
+        props: {},
         data() {
             return {}
         },
         computed: {},
         methods: {},
         components: {}
+
     }
 </script>
 
