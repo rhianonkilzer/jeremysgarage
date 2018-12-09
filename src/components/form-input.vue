5a56d94d735319ef8711d96e029cbf8a0eeb8ccf
Christopher Brown <chbphone55@gmail.com>
2018-12-09 11:32:43 -0700

    Form Input
    Co-authored-by: Rhianon Kilzer <rhianonkilzer@yahoo.com>

--- src/components/form-input.vue
+++ src/components/form-input.vue
@@ -1,19 +1,30 @@
 <template>
     <div class="form-input">
+      <label :for="id">{{label}}</label>
+      <input :id="id" :placeholder="placeholder" :name="name" :type="type" />
     </div>
 </template>
 
-
 <script>
-    export default {
-        name: 'form-input',
-        data() {
-            return {}
-        },
-        computed: {},
-        methods: {},
-        components: {}
-    }
+  let idCounter = 0;
+
+  export default {
+    name: 'form-input',
+    props: {
+      label: String,
+      placeholder: String,
+      name: String,
+      type: String
+    },
+    data() {
+      return {
+        id: idCounter++
+      };
+    },
+    computed: {},
+    methods: {},
+    components: {}
+  }
 </script>
 
 
